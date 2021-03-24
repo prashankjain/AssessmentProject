86a774f48a8ff537646dfe9a05fd6b52591016c3
Jain Prashank Kumar <Prashank_kumar_Jain@Dell.com>
2021-03-24 22:52:08 +0530

    first commit

---
+++ Storing_Updating_Teacher_Data/Storing_Updating_Teacher_Data/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Storing_Updating_Teacher_Data
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TeachersDataModification teachersData = new TeachersDataModification();
+            int option = 0;
+            TeacherDetail teacherDetail = new TeacherDetail();
+//            int ID; string Name; string className; char section;
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("THis is a Rainbow School Teacher's Data Management Application.");
+            Console.WriteLine("----------------------------------------------------------------");
+            Console.WriteLine("Select an option to perform:");
+            Console.WriteLine("1. storing the teacher's data");
+            Console.WriteLine("2. updating the teacher's data");
+            Console.WriteLine("3. retriving the teacher's data");
+            Console.WriteLine("Please type a number (1 or 2 or 3) to perform the action??");
+            option = Convert.ToInt32(Console.ReadLine());
+            if (option == 1)
+            {
+                Console.WriteLine("Please Enter the Teacher's data to store");
+                Console.WriteLine("Teacher ID:");
+                teacherDetail.tId = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Teacher Name:");
+                teacherDetail.tName = Console.ReadLine();
+                Console.WriteLine("Teacher Classname:");
+                teacherDetail.className = Console.ReadLine();
+                Console.WriteLine("Teacher section:");
+                teacherDetail.section = Convert.ToChar(Console.ReadLine());
+
+                Console.WriteLine("Need to call Store function");
+
+                string result = teachersData.Store(teacherDetail);
+            }
+            else if (option == 2)
+            {
+                Console.WriteLine("Please Enter the Teacher's data to update");
+                Console.WriteLine("Teacher ID:");
+                teacherDetail.tId = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Teacher Name:");
+                teacherDetail.tName = Console.ReadLine();
+                Console.WriteLine("Teacher Classname:");
+                teacherDetail.className = Console.ReadLine();
+                Console.WriteLine("Teacher section:");
+                string result = teachersData.update(teacherDetail);
+            }
+            else if (option == 3)
+            {
+                Console.WriteLine("Need to call Read function");
+            }
+            else
+            {
+                Console.WriteLine("invalid option");
+            }
+
+            Console.ReadKey();
+
+            //            StreamWriter OurStream;
+            ////          OurStream = File.CreateText("c:\\examples\\test.txt");
+            //            OurStream = File.AppendText("c:\\examples\\test.txt");
+            //            OurStream.WriteLine("This is a line of text");
+            //            OurStream.Close();
+            //            Console.WriteLine("Created File!");
+        }
+    }
+}
---
+++ Storing_Updating_Teacher_Data/Storing_Updating_Teacher_Data/TeachersDataModification.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Data;
+
+namespace Storing_Updating_Teacher_Data
+{
+    public class TeacherDetail 
+    {
+        public int tId;
+        public string tName;
+        public string className;
+        public char section;
+
+        public TeacherDetail()
+        {
+
+        }
+
+    }
+
+    
+    class TeachersDataModification
+    {
+        string filePath = "c:\\examples\\test.txt";
+        public string Store(TeacherDetail teacherDetail)
+        {
+            StreamWriter OurStream;
+            OurStream = File.AppendText(filePath);
+            OurStream.WriteLine(teacherDetail.tId + ":" + teacherDetail.tName + ":" + teacherDetail.className + ":" + teacherDetail.section);
+            //OurStream.WriteLine("\n");
+            OurStream.Close();
+            Console.WriteLine("Records is stored successfully..");
+            return "Data is stored";
+        }
+
+        public string update(TeacherDetail teacherDetail)
+        {
+
+            DataTable dt = ConvertToDataTable(filePath, 100);
+
+            foreach (var row in dt.Rows)
+            {
+                DataRow dataRow = 
+            }
+
+            return "ok";
+        }
+
+        public DataTable ConvertToDataTable(string filePath, int numberOfColumns)
+        {
+            DataTable table = new DataTable();
+            var fileContents = File.ReadAllLines(filePath);
+
+            var splitFileContents = (from f in fileContents select f.Split(':')).ToArray();
+
+            int maxLength = (from s in splitFileContents select s.Count()).Max();
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                table.Columns.Add();
+            }
+
+            foreach (var line in splitFileContents)
+            {
+                DataRow row = table.NewRow();
+                row.ItemArray = (object[])line;
+                table.Rows.Add(row);
+            }
+            //DataTable tbl = new DataTable();
+
+            //for (int col = 0; col < numberOfColumns; col++)
+            //    tbl.Columns.Add(new DataColumn("Column" + (col + 1).ToString()));
+
+
+            //string[] lines = System.IO.File.ReadAllLines(filePath);
+
+            //foreach (string line in lines)
+            //{
+            //    var cols = line.Split(':');
+
+            //    DataRow dr = tbl.NewRow();
+            //    for (int cIndex = 0; cIndex < 3; cIndex++)
+            //    {
+            //        dr[cIndex] = cols[cIndex];
+            //    }
+
+            //    tbl.Rows.Add(dr);
+            //}
+
+            return table;
+        }
+    }
+}
